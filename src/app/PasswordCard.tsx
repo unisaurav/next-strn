c43f247268f446599378a2caa0257a46538c1c7f
Saurav Kitukale <saurav.kitukale@atos.net>
2023-07-27 14:17:20 +0100

    withlogic

--- src/app/PasswordCard.tsx
+++ src/app/PasswordCard.tsx
@@ -10,82 +10,76 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 interface trainInfo {
-  [key: number]: string;
+  [key: number]: any;
 }
 
 const PasswordCard = () => {
   const [showPassword, toggleShow] = useState(false);
   const [password, changePassword] = useState("");
-  const [green, changeGreen] = useState("");
+  const [green, changeGreen] = useState({ color: `red-500`, width: `0` });
 
-  const strengthCheck = (pas: string) => {
+  useEffect(() => {
     const colorObj: trainInfo = {
-      0: `border-red-500`,
-      1: `border-orange-500`,
-      2: `border-yellow-400`,
-      3: `border-green-200`,
-      4: `border-green-800`,
+      0: { color: `red-500`, width: `0` },
+      1: { color: `orange-500`, width: `1/4` },
+      2: { color: `yellow-400`, width: `1/2` },
+      3: { color: `green-200`, width: `3/4` },
+      4: { color: `green-800`, width: "full" },
     };
     let points = 0;
     var special = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-    var caps = /^[A-Z]/;
-    var lowcCaps = /^[a-z]/;
-    if (pas.length > 10) {
+    var caps = /[A-Z]+/;
+    var lowcCaps = /^[a`-z]/;
+    if (password.length > 10) {
+      console.log("lensucc");
       points++;
     }
-    if (special.test(pas)) {
+    if (special.test(password)) {
+      console.log("special");
+
       points++;
     }
-    if (caps.test(pas)) {
+    if (caps.test(password)) {
+      console.log("caps");
+
       points++;
     }
-    if (lowcCaps.test(pas)) {
+    if (lowcCaps.test(password)) {
+      console.log("low");
+
       points++;
     }
-    console.log(points)
-
-    return colorObj[points];
-  };
-
-
-  console.log(strengthCheck(password));
-
+    console.log(points);
+    changeGreen(colorObj[points]);
+  }, [password]);
 
   return (
     <div>
-      {" "}
       <Card className="w-[350px]">
         <CardHeader>
           <CardTitle>Strength Checker</CardTitle>
-          <CardDescription>
-            Check Password stringth with just one click
-          </CardDescription>
         </CardHeader>
         <CardContent>
           <form>
-            <div className="grid w-full items-center gap-4  ">
-              <div className={`flex flex-col space-y-1.5 ${password}`}>
+            <div className="grid w-full items-center gap-4 ">
+              <div className={`flex flex-col space-y-1.5 `}>
                 <Label htmlFor="name">Password</Label>
                 <Input
                   id="name"
                   value={password}
                   type={showPassword ? "password" : "text"}
                   onChange={(e) => changePassword(e.target.value)}
-                  placeholder="Name of your project"
-                  className={`${strengthCheck(password)} `}
+                  placeholder="ABCdefc£$% "
+                  className={`border-${green.color} placeholder:text-zinc-600`}
                 />
               </div>
             </div>
           </form>
+          <div className="w-full bg-gray-200  h-1 mb-4 ">
+            <div className={`bg-${green.color} h-1  w-${green.width}`}></div>
+          </div>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline" onClick={() => toggleShow(!showPassword)}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import PasswordCard from './PasswordCard'
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import PasswordCard from "./PasswordCard";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, toggleDarkMode] = useState(false);
+
   return (
-    <main className="flex flex-row justify-center py-16">
+    <main className="flex justify-center items-center h-screen">
       <PasswordCard />
     </main>
-  )
+  );
 }
