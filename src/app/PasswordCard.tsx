b2a1ff5f94f421043873c07a56df8c4d0049c0f6
unknown <Saurav@DESKTOP-S1DGDUF>
2023-07-27 18:39:59 +0100

    minr

--- src/app/PasswordCard.tsx
+++ src/app/PasswordCard.tsx
@@ -11,17 +11,17 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState, useEffect } from "react";
-interface trainInfo {
+interface colorAndWidth {
   [key: number]: any;
 }
 
 const PasswordCard = () => {
   const [showPassword, toggleShow] = useState(false);
   const [password, changePassword] = useState("");
-  const [green, changeGreen] = useState({ color: `red-500`, width: `0` });
+  const [green, changeGreen] = useState(`0`);
 
-  useEffect(() => {
-    const colorObj: trainInfo = {
+  const onCheck = () => {
+    const colorObj: colorAndWidth = {
       0: { color: `red-500`, width: `0` },
       1: { color: `orange-500`, width: `1/4` },
       2: { color: `yellow-400`, width: `1/2` },
@@ -33,27 +33,21 @@ const PasswordCard = () => {
     var caps = /[A-Z]+/;
     var lowcCaps = /^[a`-z]/;
     if (password.length > 10) {
-      console.log("lensucc");
       points++;
     }
     if (special.test(password)) {
-      console.log("special");
-
       points++;
     }
     if (caps.test(password)) {
-      console.log("caps");
-
       points++;
     }
     if (lowcCaps.test(password)) {
-      console.log("low");
-
       points++;
     }
     console.log(points);
-    changeGreen(colorObj[points]);
-  }, [password]);
+    let value = `w-${colorObj[points].width} bg-green-800 h-1 `;
+    changeGreen(value);
+  };
 
   return (
     <div>
@@ -72,20 +66,23 @@ const PasswordCard = () => {
                   type={showPassword ? "password" : "text"}
                   onChange={(e) => changePassword(e.target.value)}
                   placeholder="ABCdefc£$% "
-                  className={`border-${green.color} placeholder:text-zinc-600`}
+                  className={`border-green-800 placeholder:text-zinc-600`}
                 />
               </div>
             </div>
           </form>
           <div className="w-full bg-gray-200  h-1 mb-4 ">
-            <div className={`bg-${green.color} h-1  w-${green.width}`}></div>
+            <div className={green}></div>
           </div>
+          <div>{green}</div>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline" onClick={() => toggleShow(!showPassword)}>
             {showPassword ? "Show Password" : "Hide Password"}
           </Button>
-          <Button>Check</Button>
+          <Button variant="outline" onClick={() => onCheck()}>
+            Check...
+          </Button>
         </CardFooter>
       </Card>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import PasswordCard from "./PasswordCard";
-import { useState } from "react";
 
 export default function Home() {
-  const [darkMode, toggleDarkMode] = useState(false);
 
   return (
     <main className="flex justify-center items-center h-screen">
