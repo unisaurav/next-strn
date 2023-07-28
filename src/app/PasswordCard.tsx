fbf6e21d6afaeca68207724c5eb745db5f4591b6
Saurav Kitukale <saurav.kitukale@atos.net>
2023-07-28 11:15:00 +0100

    tailwind fix

--- src/app/PasswordCard.tsx
+++ src/app/PasswordCard.tsx
@@ -11,48 +11,48 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState, useEffect } from "react";
-interface trainInfo {
+interface colorType {
   [key: number]: any;
 }
 
-const PasswordCard = () => {
+const PasswordCard =() => {
   const [showPassword, toggleShow] = useState(false);
   const [password, changePassword] = useState("");
   const [green, changeGreen] = useState({ color: `red-500`, width: `0` });
 
   useEffect(() => {
-    const colorObj: trainInfo = {
-      0: { color: `red-500`, width: `0` },
-      1: { color: `orange-500`, width: `1/4` },
-      2: { color: `yellow-400`, width: `1/2` },
-      3: { color: `green-200`, width: `3/4` },
-      4: { color: `green-800`, width: "full" },
+    const colorObj: colorType = {
+      0: { color: `bg-red-500`, width: `w-0` },
+      1: { color: `bg-orange-500`, width: `w-1/4` },
+      2: { color: `bg-yellow-400`, width: `w-1/2` },
+      3: { color: `bg-green-200`, width: `w-3/4` },
+      4: { color: `bg-green-800`, width: "w-full" },
     };
-    let points = 0;
+    let strength = 0;
     var special = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     var caps = /[A-Z]+/;
     var lowcCaps = /^[a`-z]/;
     if (password.length > 10) {
       console.log("lensucc");
-      points++;
+      strength++;
     }
     if (special.test(password)) {
       console.log("special");
 
-      points++;
+      strength++;
     }
     if (caps.test(password)) {
       console.log("caps");
 
-      points++;
+      strength++;
     }
     if (lowcCaps.test(password)) {
       console.log("low");
 
-      points++;
+      strength++;
     }
-    console.log(points);
-    changeGreen(colorObj[points]);
+    console.log(strength);
+    changeGreen(colorObj[strength]);
   }, [password]);
 
   return (
@@ -78,14 +78,13 @@ const PasswordCard = () => {
             </div>
           </form>
           <div className="w-full bg-gray-200  h-1 mb-4 ">
-            <div className={`bg-${green.color} h-1  w-${green.width}`}></div>
+            <div className={`${green.color} h-1  ${green.width}`}></div>
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline" onClick={() => toggleShow(!showPassword)}>
             {showPassword ? "Show Password" : "Hide Password"}
           </Button>
-          <Button>Check</Button>
         </CardFooter>
       </Card>
     </div>
